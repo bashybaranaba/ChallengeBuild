90b3a7b5c3ae89bc3721e15877f4ba12fb2fccab
Baranaba <baranaba@vokali.com>
2022-10-19 22:24:31 +0300

    first commit

---
+++ precache-manifest.f33837b74021792db3a00597c5d079fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee5c65f2c0dc00afdb54",
+    "url": "/static/css/main.a7c8e964.chunk.css"
+  },
+  {
+    "revision": "ee5c65f2c0dc00afdb54",
+    "url": "/static/js/main.4ba83085.chunk.js"
+  },
+  {
+    "revision": "b605d26d900a43fa76a2",
+    "url": "/static/js/runtime~main.4ef16878.js"
+  },
+  {
+    "revision": "96db6bdbbf46ede9deb3",
+    "url": "/static/css/2.fbadb3a0.chunk.css"
+  },
+  {
+    "revision": "96db6bdbbf46ede9deb3",
+    "url": "/static/js/2.58ccfbe3.chunk.js"
+  },
+  {
+    "revision": "af92fa87062199ff68f9",
+    "url": "/static/js/3.2cba1d63.chunk.js"
+  },
+  {
+    "revision": "a2398eefc5c78a2518f6169761e5e8ac",
+    "url": "/static/media/logo.a2398eef.png"
+  },
+  {
+    "revision": "910d431a1e6306178bfc5c624b62c01c",
+    "url": "/static/media/signinbg.910d431a.png"
+  },
+  {
+    "revision": "48b6812fef781b0a44e75b0a8ad637c0",
+    "url": "/static/media/Registrationbg.48b6812f.png"
+  },
+  {
+    "revision": "be73c5373704536d51e0bdc068a55513",
+    "url": "/static/media/happyman.be73c537.png"
+  },
+  {
+    "revision": "cd7f1efa782e4027340dba9ec006d884",
+    "url": "/static/media/coolman.cd7f1efa.png"
+  },
+  {
+    "revision": "947ca2e11325299de3debe62ccfa86ca",
+    "url": "/static/media/jumpingman.947ca2e1.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5fda02a1c33a988f19262c2e085252dd",
+    "url": "/index.html"
+  }
+];
